346db75a0102961bfb0d4a5f7f13b009c1b8cdf3
J. Oppenlaender <j.oppenlaender@web.de>
2016-03-01 19:21:51 +0000

    installation files

---
+++ install/create_sqlite_table.sql
@@ -0,0 +1,20 @@
+create table if not exists table1 (canon TEXT, symbol TEXT PRIMARY KEY) WITHOUT ROWID;
+create index if not exists table1_idx on table1 (canon);
+insert into table1 values("http://www.wikidata.org/entity/Q23436", "http://www.wikidata.org/entity/Q23436");
+insert into table1 values("http://www.wikidata.org/entity/Q23436", "http://dbpedia.org/resource/Embra");
+insert into table1 values("http://www.wikidata.org/entity/Q23436", "http://data.nytimes.com/edinburgh_scotland_geo");
+insert into table1 values("http://www.wikidata.org/entity/Q23436", "http://sws.geonames.org/2650225/");
+insert into table1 values("http://www.wikidata.org/entity/Q23436", "http://data.ordnancesurvey.co.uk/id/50kGazetteer/81482");
+
+insert into table1 values("http://www.wikidata.org/entity/Q220966", "http://www.wikidata.org/entity/Q220966");
+insert into table1 values("http://www.wikidata.org/entity/Q220966", "http://dbpedia.org/resource/Soton");
+insert into table1 values("http://www.wikidata.org/entity/Q220966", "http://data.nytimes.com/southampton_england_geo");
+insert into table1 values("http://www.wikidata.org/entity/Q220966", "http://data.ordnancesurvey.co.uk/id/50kGazetteer/218013");
+insert into table1 values("http://www.wikidata.org/entity/Q220966", "http://sws.geonames.org/1831142/");
+
+insert into table1 values("http://www.wikidata.org/entity/Q6940372", "http://www.wikidata.org/entity/Q6940372");
+insert into table1 values("http://www.wikidata.org/entity/Q6940372", "http://dbpedia.org/resource/Manc");
+insert into table1 values("http://www.wikidata.org/entity/Q6940372", "http://data.nytimes.com/manchester_england_geo");
+insert into table1 values("http://www.wikidata.org/entity/Q6940372", "http://data.ordnancesurvey.co.uk/id/50kGazetteer/155254");
+insert into table1 values("http://www.wikidata.org/entity/Q6940372", "http://sws.geonames.org/524894/");
+
